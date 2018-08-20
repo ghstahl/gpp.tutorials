32a6c14c624424c7bf783baee102dabc79723ad2
Herb Stahl <ghstahl@gmail.com>
2018-08-20 14:19:28 -0700

    reference: https://www.youtube.com/watch?v=DIw02CaEusY
    starter to make sure you can compile and debug

---
+++ src/001.HelloWorld/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(){
+    int x =10;
+    int y =2;
+    int product = x*y;
+    std::cout << "hello world!"  << std::endl;
+    std::cout << x <<"+"<< y<<"="<< product<< std::endl;
+    system("pause");
+}
