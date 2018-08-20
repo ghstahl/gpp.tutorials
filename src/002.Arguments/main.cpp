dd5d6b12b1e3d102cbc9245f03379167860625a4
Herb Stahl <ghstahl@gmail.com>
2018-08-20 15:37:22 -0700

    Update main.cpp

--- src/002.Arguments/main.cpp
+++ src/002.Arguments/main.cpp
@@ -9,10 +9,14 @@ you put "" around anything that has a space in it.
 
 output
 
-H:\Github\ghstahl\gpp.tutorials\src\001.HelloWorld>.\a.exe
-hello world!
-10+2=20
-Press any key to continue . . .
+You have entered 6 arguments:
+arg[0]: a.exe
+arg[1]: a
+arg[2]: -s
+arg[3]: 1
+arg[4]: 2.34
+arg[5]: I like cheese, I just do
+NOTE: every argument is a string, even though some look like they are numbers.
 
 */
 
