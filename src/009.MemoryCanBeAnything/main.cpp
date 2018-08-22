a2c1054ca445648633fccf46610c25963b411888
Herb Stahl <ghstahl@gmail.com>
2018-08-22 10:11:00 -0700

    print out zeros and ones

--- src/009.MemoryCanBeAnything/main.cpp
+++ src/009.MemoryCanBeAnything/main.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <sstream>
 #include <cstring>
+#include <bitset>
 std::string ConvertToHex(int val)
 {
     std::stringstream stream;
@@ -22,13 +23,29 @@ int main(int argc, char *argv[])
         << "sizeof(char):" << sizeof(char) << std::endl
         << std::endl;
 
+    char theChar = 'D';
+    std::bitset<8> xD(theChar);
+    std::cout << xD << std::endl;
+
+    theChar = 'a';
+    std::bitset<8> xA(theChar);
+    std::cout << xA << std::endl;
+
     // a char is size 1
     // an int is size 4
     // hence if we allocate 4 chars, which will be inorder, then it
     // is big enough for an int
     std::allocator<char> alloc;
     char *buffer = alloc.allocate(4);
+
+    
+    std::bitset<32> xBuffer(*buffer);
+    std::cout << xBuffer << std::endl;
+
     std::strcpy(buffer, "hi");
+
+    xBuffer = std::bitset<32>(*buffer);
+    std::cout << xBuffer << std::endl;
 
     // now lets have an int pointer point to the start of the memory
     int *intPtr = (int *)buffer;
@@ -43,6 +60,9 @@ int main(int argc, char *argv[])
     *intPtr = 7;
     int sum = *intPtr + 3;
     *intPtr = sum;
+    xBuffer = std::bitset<32>(*buffer);
+    std::cout << xBuffer << std::endl;
+    std::cout << "That is sure a lot of zeros and ones just to represent 10"<< std::endl;
     std::cout
         << "Interpretation Matters." << std::endl
         << "========================================" << std::endl
@@ -56,7 +76,7 @@ int main(int argc, char *argv[])
     // 0010 = 2
     // ...
     // 1010 = 10
-    // a bunch of 0's are in front of that 1010, and a string goes until it hits 
+    // a bunch of 0's are in front of that 1010, and a string goes until it hits
     // what it interprets as a null terminator.
     // in this case the null terminator is at the very start, hence it still prints buffer as a string
     // buffer is just empty
